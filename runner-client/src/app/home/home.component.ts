f89b09eb0366dc3f6b4a12247fd901ed8274d36b
jonatantao <michelik.jony@gmail.com>
2022-05-02 19:56:29 +0200

    FE UPDATE

--- runner-client/src/app/home/home.component.ts
+++ runner-client/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../_services/user.service';
+import {TokenStorageService} from '../_services/token-storage.service';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,9 @@ import { UserService } from '../_services/user.service';
 })
 export class HomeComponent implements OnInit {
   content: string;
+  currentUser: any;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private token: TokenStorageService) { }
 
   ngOnInit() {
     this.userService.getPublicContent().subscribe(
@@ -20,5 +22,6 @@ export class HomeComponent implements OnInit {
         this.content = JSON.parse(err.error).message;
       }
     );
+    this.currentUser = this.token.getUser();
   }
 }
